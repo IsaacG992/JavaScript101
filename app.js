2761c608edb0403ea04bcbd69077ea37c03f8868
unknown <gunnisaac10@gmail.com>
2024-03-13 14:32:48 -0400

    initial commit

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
 /////////////////////////
-// Part 2 ""//
+// Part 2 "Math Problems"//
 /////////////////////////
 
 // 1.
@@ -74,3 +74,54 @@ console.log(multiplyResult)
 //6. 
 const isOver25Number = (n1 <= 25 && n2 <= 25 && n3 <= 25 && n4 <= 25);
 console.log(isOver25);
+
+////////////////////////////
+// Part 3 "Practical Math"//
+////////////////////////////
+
+//1. 
+let tripDistance = 1500;
+let mph55 = 30;
+let mph60 = 28;
+let mph75 = 23;
+let budget = 175;
+let avgCost = 3;
+// 55 mph
+let total55gal = tripDistance / mph55;
+console.log(total55gal)
+
+let totalBudget55 = total55gal * avgCost;
+console.log(totalBudget55);
+
+let overBudget55 = budget >= totalBudget55;
+console.log(overBudget55)
+
+let tripTime55 = tripDistance / 55;
+console.log(tripTime55)
+
+// 60 mph
+let total60gal = tripDistance / mph60;
+console.log(total60gal)
+
+let totalBudget60 = total60gal * avgCost;
+console.log(totalBudget60);
+
+let overBudget60 = budget >= totalBudget60;
+console.log(overBudget60)
+
+let tripTime60 = tripDistance / 60;
+console.log(tripTime60)
+// 75 mph
+let total75gal = tripDistance / mph75;
+console.log(total75gal)
+
+let totalBudget75 = total75gal * avgCost;
+console.log(totalBudget75);
+
+let overBudget75 = budget >= totalBudget75;
+console.log(overBudget75)
+
+let tripTime75 = tripDistance / 75;
+console.log(tripTime75)
+
+// 60 mp would be the best option
